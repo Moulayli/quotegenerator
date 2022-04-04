9acc6d91d5165ba79e859f3fc1b36047f89cb69a
Moulayli <m2xyoussouf@gmail.com>
2022-04-04 16:34:20 +0200

    final version

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button,ImageBackground } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+   const [quote ,setQuote]=useState()
+  const onPressLearnMore = ()=>{
+    fetch('https://luha.alwaysdata.net/api/')
+    .then((res)=>{
+      return res.json()
+    }).
+    then((data)=>{
+      setQuote(data.citation)
+    })
+  }
+  const image = { uri: "https://images2.alphacoders.com/788/thumb-1920-788976.jpg" };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+
+      <Text style={styles.text} >{quote}</Text>
+      <Button
+        style={styles.button}
+        borderRadius='100'
+        onPress={onPressLearnMore}
+        title="Citation"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +38,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color:'white',
+    //height:'500',
+  },
+  text: {
+    color: "white",
+    fontSize:15,
+    lineHeight: 15,
+    fontWeight: "bold",
+    padding:25,
+    textAlign: "center",
+    backgroundColor: "#000000c1"
+  },
+  button: {
+    color: "red",
+    backgroundColor:'red',
+    borderRadius:25,
+
+  },
+  image: {
+    flex: 1,
+    height:'100%',
+    width:'100%',
+    justifyContent: "center"
   },
 });
